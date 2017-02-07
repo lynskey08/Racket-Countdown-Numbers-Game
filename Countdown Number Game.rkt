7742b69b131a134db5057971bd6c2c625545625b
Gareth Lysnkey <lynskey123@gmail.com>
2017-02-07 18:35:24 +0000

    List of Numbers
    Here i defined a list called num, which contains all the numbers that
    can be used in the calculation of the sum

---
+++ Countdown Number Game.rkt
@@ -0,0 +1,6 @@
+#lang racket
+
+; Defined a list called num that contains the numbers that can be used in
+; the calcuation of the sum
+(define num (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+num
