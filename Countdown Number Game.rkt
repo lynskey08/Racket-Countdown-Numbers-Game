cb1350e2cb10c471e203f73f08198fd92a6a4e56
Gareth Lysnkey <lynskey123@gmail.com>
2017-02-22 21:46:43 +0000

    Display comments added to output

--- Countdown Number Game.rkt
+++ Countdown Number Game.rkt
@@ -3,14 +3,16 @@
 ; Defined a list called num that contains the numbers that can be used in
 ; the calcuation of the sum
 (define numList (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
-numList
+(display "This is the range of that 6 randomly generated numbers will be chosen from: ")numList
+(display "\n")
 
 ;Random number generator - picks a random number between the values 101 and 999
 ;I set the max number as 1000 and not 999 because the set value cannot be chosen
 ; randomly and in case we want to use 999. I did not change the min number because
 ; the min number can be chosen randomly. 
 (define targetNumber (random 101 1000))
-targetNumber
+(display "This is the target number: ")targetNumber
+
 
 ;Defined an empty list to store the 6 random numbers from the list numList
 (define numSelection (list))
