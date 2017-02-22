1768b7f4766365176479b6cda34fdf5fe16fc514
Gareth Lysnkey <lynskey123@gmail.com>
2017-02-22 17:12:29 +0000

    6 random numbers are generated from the list numList.

--- Countdown Number Game.rkt
+++ Countdown Number Game.rkt
@@ -5,29 +5,30 @@
 (define numList (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 numList
 
-; random number generator - picks a random number between the values 101 and 999
-; I set the max number as 1000 and not 999 because the set value cannot be chosen
+;Random number generator - picks a random number between the values 101 and 999
+;I set the max number as 1000 and not 999 because the set value cannot be chosen
 ; randomly and in case we want to use 999. I did not change the min number because
 ; the min number can be chosen randomly. 
 (define targetNumber (random 101 1000))
 targetNumber
 
-; defined an empty list to store the 6 random numbers from the list numList
+;Defined an empty list to store the 6 random numbers from the list numList
 (define numSelection (list))
 
 
-; defines the function that takes in the variable l(basically the list of numbers)
+;Defines the function that takes in the variable l(basically the list of numbers)
 (define (randomNumList l)
-  ; define the function randomNumber that uses list-ref to get a certain position in
+  ;Define the function randomNumber that uses list-ref to get a certain position in
   ; a list. In this case I use it to find a certain random number in the list l.
-  (define randomNumber(list-ref l(random (length l)))
-  ;set the list l and removes the random number from the list l that has already been selected to ensure that it cannot be selected again
+  (define randomNumber(list-ref l(random (length l))))
+  ;Removes the random number from the list l that has already been selected to ensure that it cannot be selected again
   (set! l(remove randomNumber l))
+  ;Add the random number to the list numSelection
+  (set! numSelection (cons randomNumber numSelection))
+  ;if the length of l is 6
   (if (= (length numSelection) 6)
-      numSelection
-      (randomNumList l)))
-
-
+      numSelection ;print numSelection
+      (randomNumList l))) ;else call the function again with the updated l list
 
 
 ; the list numList is passed into this empty list randomNumList
