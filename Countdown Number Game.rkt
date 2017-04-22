ac65ee9ae6137b22db91da037d48ef735b8e887b
Gareth Lysnkey <lynskey123@gmail.com>
2017-04-22 16:06:14 +0100

    Playing around with code from the labs

--- Countdown Number Game.rkt
+++ Countdown Number Game.rkt
@@ -12,30 +12,44 @@
 ; the min number can be chosen randomly. 
 (define targetNumber (random 101 1000))
 (display "This is the target number: ")targetNumber
-
+(display "This is the randomly selected range of numbers you can use: ")
 
 ;Defined an empty list to store the 6 random numbers from the list numList
 (define numSelection (list))
-
 
 ;Defines the function that takes in the variable l(basically the list of numbers)
 (define (randomNumList l)
   ;Define the function randomNumber that uses list-ref to get a certain position in
   ; a list. In this case I use it to find a certain random number in the list l.
   (define randomNumber(list-ref l(random (length l))))
-  ;Removes the random number from the list l that has already been selected to ensure that it cannot be selected again
+  ;Removes the random number from the list l that has already been selected to
+  ;ensure that it cannot be selected again
   (set! l(remove randomNumber l))
   ;Add the random number to the list numSelection
   (set! numSelection (cons randomNumber numSelection))
   ;if the length of l is 6
   (if (= (length numSelection) 6)
-      numSelection ;print numSelection
+     numSelection ;print numSelection
       (randomNumList l))) ;else call the function again with the updated l list
-
 
 ; the list numList is passed into this empty list randomNumList
 (randomNumList numList)
 
+; the algorithm
+(display "\n")
+(display "\n")
+(display "\n")
 
 
-  
+(define start(list -1 -1 -1 -1 1 1 1 1))
+(define getPerms(remove-duplicates (permutations start)))
+(define (to-rpn l)
+  (append (list 1 1) l (list -1)))
+(map to-rpn getPerms)
+
+
+
+
+
+
+
